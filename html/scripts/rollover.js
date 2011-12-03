556c5546e3ebec5068ec4c6133370c764fcf4de0
Timur Sufiev <tsufiev@gmail.com>
2011-12-03 16:18:38 +0400

    Fix javascript selection of links.

--- html/scripts/rollover.js
+++ html/scripts/rollover.js
@@ -12,11 +12,16 @@ function add_rollover(img, rollover_url) {
     }
 
     if ( img.tagName.toLowerCase() != "img" ) return;
-    var base_url = img.src;
-    (new Image()).src = rollover_url;
+    img.base_url = img.src;
+    (new Image()).src = img.rollover_url = rollover_url;
+}
 
-    img.onmouseover = function() { img.src = rollover_url; }
-    img.onmouseout = function() { img.src = base_url; }
+function make_images_changer(images, property) {
+    return function () {
+        for ( var j = 0; j < images.length; j++ ) {
+            images[j].src = images[j][property];
+        }
+    }
 }
 
 function init_rollovers() {
@@ -26,6 +31,30 @@ function init_rollovers() {
         var rollover_url = image.getAttribute("rollover");
         if ( rollover_url )
             add_rollover(image, rollover_url);
+    }
+    
+    var objs = {};
+    var links = document.getElementsByTagName("a");
+    for ( var i = 0; i < links.length; i++ ) {
+        var href = links[i].href;
+        if ( href ) {
+            var child = links[i].children[0];
+            if ( child && child.tagName.toLowerCase() == "img" ) {
+                if ( objs[href] )
+                    objs[href].push(child);
+                else
+                    objs[href] = [child];
+            }
+        }
+    }
+
+    for ( var href in objs ) {
+        for ( var i = 0; i < objs[href].length; i++ ) {
+            with ( objs[href][i] ) {
+                onmouseover = make_images_changer(objs[href], "rollover_url");
+                onmouseout = make_images_changer(objs[href], "base_url");
+            }
+        }
     }
 }
 
