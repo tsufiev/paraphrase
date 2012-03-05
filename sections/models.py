51ddc69bfca86bb7b98ca2f839d646194b6a9f90
Timur Sufiev <tsufiev@gmail.com>
2012-03-05 15:30:22 +0400

    improve list of objects in admin.

--- sections/models.py
+++ sections/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib import admin
 
 class Entry(models.Model):
-    text = models.TextField(u'Отзыв')
+    text = models.TextField(u'Текст')
     published_on = models.DateTimeField(auto_now_add=True)
     is_visible = models.BooleanField(default=True)
 
@@ -26,17 +26,30 @@ from photologue.models import Photo
 class Actor(Photo):
     pass
 
+def cut_text(text):
+    if len(text) < 20:
+        return text
+    else:
+        return u'%s...'%text[0:50]
+
 class Announcement(Entry):
-    pass
+    def __unicode__(self):
+        return cut_text(self.text)
 
 class Contact(Entry):
-    pass
+    def __unicode__(self):
+        return cut_text(self.text)
 
 class Video(Entry):
     url = models.URLField()
+    def __unicode__(self):
+        return cut_text(self.text)
 
 class Feedback(Entry):
     author = models.CharField(u'Автор',max_length=50)
+    def __unicode__(self):
+        timestamp = self.published_on.strftime('%d.%m.%Y %H:%M')
+        return u'Отзыв от %s, автор: %s'% (timestamp, self.author)
 
 class TitledEntryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'title_slug': ('title',)}
