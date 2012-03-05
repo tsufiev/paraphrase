92e91c9811310608a402863e62af7b5dec1c7d53
Timur Sufiev <tsufiev@gmail.com>
2012-03-05 19:51:24 +0400

    split entries to pages inside show_section.

--- views.py
+++ views.py
@@ -7,7 +7,11 @@ from photologue.models import Photo, Gallery
 from sections.views import find_section, get_section_ids
 from sections.models import Feedback
 
-def show_section(request, section_id = 'home', template = None, context = {}):
+def show_section(request, 
+                 section_id = 'home', 
+                 template = None, 
+                 context = {}, 
+                 page = None):
     section = find_section(section_id)
     model = section['model']
     context = dict({'sections': get_section_ids(),
@@ -15,6 +19,12 @@ def show_section(request, section_id = 'home', template = None, context = {}):
                     'current_section': section_id,
                     'entries': model and model.objects.all(),
                     }.items() + context.items())
+    pages = make_pages(context['entries'], section_id)
+    if page and pages:
+        try:
+            context['entries'] = pages.page(int(page))
+        except EmptyPage:
+            raise Http404
     context.update(csrf(request))
     return render_to_response(template or '%s.html'%section_id, context)
 
@@ -54,9 +64,13 @@ def show_photo(request, photo, gallery = None):
             'prev_in_gallery': photo.get_previous_in_gallery(gallery_obj), 
             })
         
+from django.http import HttpResponse
 def make_pages(objects, section_id = 'photos'):
-    return Paginator(objects,
-                     find_section(section_id)['objs_per_page'])
+    objs_per_page = find_section(section_id)['objs_per_page']
+    if objs_per_page:
+        return Paginator(objects, objs_per_page)
+    else:
+        return None
 
 def get_page_number(paginator, object):
     for page in paginator.page_range:
@@ -69,12 +83,11 @@ def get_gallery_page_number(gallery, photo):
 def show_gallery(request, slug = None, page = None):
     if slug:
         gallery = get_object_or_404(Gallery, title_slug=slug)
-        paginator = make_pages(gallery.photos.order_by('id'))
+        entries = gallery.photos.order_by('id')
     else:
-        paginator = make_pages(Gallery.objects.all())
-    try:
-        return show_section(request, 'photos', 'gallery.html', 
-                            {'objects': paginator.page(int(page)),
-                             'current_gallery': slug})
-    except EmptyPage:
-        raise Http404
+        entries = Gallery.objects.all()
+    return show_section(request, 'photos', 'gallery.html', 
+                        {'entries': entries,
+                         'page': page,
+                         'current_gallery': slug},
+                        page)
