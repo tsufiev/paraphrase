8a696139d402d9d240ad89e2c0d7a7e8bbab65be
Timur Sufiev <tsufiev@gmail.com>
2012-03-05 19:18:36 +0400

    store objs_per_page property for each model/section.

--- views.py
+++ views.py
@@ -7,8 +7,6 @@ from photologue.models import Photo, Gallery
 from sections.views import find_section, get_section_ids
 from sections.models import Feedback
 
-OBJS_PER_PAGE = 12
-
 def show_section(request, section_id = 'home', template = None, context = {}):
     section = find_section(section_id)
     model = section['model']
@@ -28,10 +26,6 @@ class FeedbackForm(ModelForm):
 
 from django.core.context_processors import csrf
 from django.template import RequestContext
-
-def render_page(request, template, context):
-    return render_to_response(template, context, 
-                              context_instance = RequestContext(request))
 
 def show_feedbacks(request):
     if request.method == 'POST':
@@ -60,8 +54,9 @@ def show_photo(request, photo, gallery = None):
             'prev_in_gallery': photo.get_previous_in_gallery(gallery_obj), 
             })
         
-def make_pages(objs):
-    return Paginator(objs, OBJS_PER_PAGE)
+def make_pages(objects, section_id = 'photos'):
+    return Paginator(objects,
+                     find_section(section_id)['objs_per_page'])
 
 def get_page_number(paginator, object):
     for page in paginator.page_range:
@@ -69,7 +64,7 @@ def get_page_number(paginator, object):
             return page
 
 def get_gallery_page_number(gallery, photo):
-        return get_page_number(make_pages(gallery.photos.order_by('id')), photo)
+    return get_page_number(make_pages(gallery.photos.order_by('id')), photo)
 
 def show_gallery(request, slug = None, page = None):
     if slug:
