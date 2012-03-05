659c69d85cc12734314ca92e7e3e8e15863024cd
Timur Sufiev <tsufiev@gmail.com>
2012-03-05 21:53:38 +0400

    fix formatting in rollover.js

--- static/js/rollover.js
+++ static/js/rollover.js
@@ -51,8 +51,10 @@ function init_rollovers() {
 
     for ( var href in objs ) {
         for ( var i = 0; i < objs[href].length; i++ ) {
-            objs[href][i].onmouseover = make_images_changer(objs[href], "rollover_url");
-            objs[href][i].onmouseout = make_images_changer(objs[href], "base_url");
+            objs[href][i].onmouseover = 
+                make_images_changer(objs[href], "rollover_url");
+            objs[href][i].onmouseout = 
+                make_images_changer(objs[href], "base_url");
         }
     }
 }
